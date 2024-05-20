93d6d6641bd21664c428348c1e954d5a32831edc
Ridhorzqullah <ridhorizqullah3@gmail.com>
2024-05-20 11:44:04 +0700

    agregation

--- agregation/agregation.cpp
+++ agregation/agregation.cpp
@@ -16,4 +16,15 @@ int main() { //agregas1
 	varIbu->tambahAnak(varAnak2);
 	varibu2->tambahAnak(varAnak3);
 	varibu2->tambahAnak(varAnak1);
+
+	varIbu->cetakAnak();
+	varibu2->cetakAnak();
+
+	delete varIbu;
+	delete varibu2;
+	delete varAnak1;
+	delete varAnak2;
+	delete varAnak3;
+
+	return 0;
 };
