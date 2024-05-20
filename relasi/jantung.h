5fa3bba5b711561beb8ecf99df74c0f4ea8b1d3c
Ridhorzqullah <ridhorizqullah3@gmail.com>
2024-05-20 11:00:51 +0700

    header manusia

--- relasi/jantung.h
+++ relasi/jantung.h
@@ -4,21 +4,14 @@
 using namespace std;
 
 class jantung {
-
+public:
+	jantung() {
+		cout << "jantung dihidupkan\n";
+	}
+	~jantung() {
+		cout << "jantung dimatikan\n";
+	}
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 #endif
 
