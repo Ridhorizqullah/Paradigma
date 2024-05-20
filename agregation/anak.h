2380ac3690a1f77fdf25f29ae4d3fc5e72821add
Ridhorzqullah <ridhorizqullah3@gmail.com>
2024-05-20 11:19:35 +0700

    header anak

--- agregation/anak.h
+++ agregation/anak.h
@@ -1,3 +1,14 @@
 #ifndef ANAK_H
 #define ANAK_H
-using namespace std;
+
+class anak {
+public:
+	string nama;
+	anak(string pNama) :nama(pNama) {
+		cout << "Anak \"" << nama << "\" ada\n";
+	}
+	`anak() {
+		cout << "Anak \"" << nama << "\" tidak ada\n";
+	}
+};
+#endif
